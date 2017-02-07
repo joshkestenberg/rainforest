047fcdb527c721da04f7dd1ed96957935c32628e
joshkestenberg <joshkestenberg@gmail.com>
2017-02-06 19:43:34 -0500

    log in functional

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
   resources :products
-
   root "products#index"
 
   resources :users, only: [:new, :create]
+
+  resources :sessions, only: [:new, :create, :destroy]
 end
